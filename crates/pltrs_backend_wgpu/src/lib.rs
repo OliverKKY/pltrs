9b23e6f8e8c7ea39bab1d3f9a52f7086e78a4a39
OliverKKY <oklink@seznam.cz>
2025-12-01 12:16:10 +0100

    Add render pipeline functionality and rendering a triangle

--- crates/pltrs_backend_wgpu/src/lib.rs
+++ crates/pltrs_backend_wgpu/src/lib.rs
@@ -15,6 +15,9 @@ pub struct WgpuBackend {
     size: winit::dpi::PhysicalSize<u32>,
     surface: wgpu::Surface<'static>,
     surface_format: wgpu::TextureFormat,
+    config: wgpu::SurfaceConfiguration,
+    is_surface_configured: bool,
+    render_pipeline: wgpu::RenderPipeline,
 }
 
 impl WgpuBackend {
@@ -38,6 +41,62 @@ impl WgpuBackend {
             .find(|f| f.is_srgb())
             .copied()
             .unwrap_or(surface_caps.formats[0]);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+        let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main",
+                buffers: &[],
+                compilation_options: wgpu::PipelineCompilationOptions::default(),
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+                compilation_options: wgpu::PipelineCompilationOptions::default(),
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                polygon_mode: wgpu::PolygonMode::Fill,
+                // Requires Features::DEPTH_CLIP_CONTROL
+                unclipped_depth: false,
+                // Requires Features::CONSERVATIVE_RASTERIZATION
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            multiview: None,
+        });
 
         let backend = Self {
             window,
@@ -46,6 +105,9 @@ impl WgpuBackend {
             size,
             surface,
             surface_format,
+            config,
+            is_surface_configured: false,
+            render_pipeline,
         };
         backend.configure_surface();
         backend
@@ -80,8 +142,8 @@ impl WgpuBackend {
 
         {
             // genius usage of scopes here
-            let _rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                label: None,
+            let mut _rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
                 color_attachments: &[Some(wgpu::RenderPassColorAttachment {
                     view: &texture_view,
                     resolve_target: None,
@@ -99,6 +161,8 @@ impl WgpuBackend {
                 timestamp_writes: None,
                 occlusion_query_set: None,
             });
+            _rpass.set_pipeline(&self.render_pipeline);
+            _rpass.draw(0..3, 0..1);
         }
 
         self.queue.submit([encoder.finish()]);
@@ -136,6 +200,12 @@ impl RenderBackend for WgpuBackend {
         // nothing yet
     }
     fn resize(&mut self, width: u32, height: u32) {
+        if width > 0 && height > 0 {
+            self.config.width = width;
+            self.config.height = height;
+            self.surface.configure(&self.device, &self.config);
+            self.is_surface_configured = true;
+        }
         self.size = winit::dpi::PhysicalSize { width, height };
         self.configure_surface();
     }
